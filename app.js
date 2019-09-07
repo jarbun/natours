0a4546635438391764bedf1ab06c01063c8f53fd
jarbun <jarbun306@gmail.com>
2019-09-07 11:36:24 +0530

    Handle basic POST request

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     data: { tours }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev/data/tours-simple.json`, JSON.stringify(tours), err => {
+    // 201: Created
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
   });
 });
 
