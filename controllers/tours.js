95c660e4ad0d05147873e9a04a7ee01f5b9bec1b
jarbun <jarbun306@gmail.com>
2019-09-17 22:57:18 +0530

    Implement createTour logic

--- controllers/tours.js
+++ controllers/tours.js
@@ -1,33 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const Tour = require('../models/tours');
 
-const filepath = path.join(__dirname, '../dev', 'data', 'tours-simple.json');
-const tours = JSON.parse(fs.readFileSync(filepath));
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-exports.checkID = (req, res, next, val) => {
-  const id = Number(val);
-  const tour = tours.find(el => el.id === id);
-  
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
     });
-  }
-
-  next();
-}
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    // 400: Bad Request
-    return res.status(400).json({
+  } catch (err) {
+    res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price'
+      message: err
     })
   }
-
-  next();
 }
 
 exports.getAllTours = (req, res) => {
@@ -41,23 +29,6 @@ exports.getTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: { tour }
-  });
-}
-
-exports.createTour = (req, res) => {
-  // console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev/data/tours-simple.json`, JSON.stringify(tours), err => {
-    // 201: Created
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    });
   });
 }
 
