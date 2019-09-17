88a94c6b538904afe9dc749476b1231411623e53
jarbun <jarbun306@gmail.com>
2019-09-17 23:05:32 +0530

    Implement getTour and getAllTours logic

--- controllers/tours.js
+++ controllers/tours.js
@@ -18,18 +18,36 @@ exports.createTour = async (req, res) => {
   }
 }
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: { tours }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: { tours }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 }
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: { tour }
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 }
 
 exports.updateTour = (req, res) => {
