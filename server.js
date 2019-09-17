f79f458db4c049d888723a17099fe1b9fbca204e
jarbun <jarbun306@gmail.com>
2019-09-17 21:44:11 +0530

    Connect mongoose to database

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to DB'));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
