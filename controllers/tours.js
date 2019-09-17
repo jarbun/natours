65389d55c9b852e1e4c3e47bac5e92593a23d732
jarbun <jarbun306@gmail.com>
2019-09-17 23:16:07 +0530

    Implement updateTour and deleteTour logic

--- controllers/tours.js
+++ controllers/tours.js
@@ -50,19 +50,38 @@ exports.getTour = async (req, res) => {
   }
 }
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour>'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // Returns updated document
+      runValidators: true // Validates the update operation against model schema
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 }
 
-exports.deleteTour = (req, res) => {
-  // 204: No Content
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    
+    // 204: No Content
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 }
