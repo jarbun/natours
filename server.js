9defc1250d55a40b5a6034f723ce2fa9cbe1d2bf
jarbun <jarbun306@gmail.com>
2019-09-17 21:30:15 +0530

    Add dotenv

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
