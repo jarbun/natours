97035f08bd7bff4d19c41b675d63af8d73b780e9
jarbun <jarbun306@gmail.com>
2019-09-25 10:21:39 +0530

    Add query field limiting

--- controllers/tours.js
+++ controllers/tours.js
@@ -40,6 +40,15 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      // Exclude __v by default
+      query = query.select('-__v');
+    }
+
     // Execute query
     const tours = await query;
 
