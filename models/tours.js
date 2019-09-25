51d0da8754d3d2427390c0a37d1aefc0e1eaae9e
jarbun <jarbun306@gmail.com>
2019-09-25 17:35:16 +0530

    Add validators

--- models/tours.js
+++ models/tours.js
@@ -4,8 +4,10 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: true,
-    unique: [true, 'A tour must have a name']
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    maxlength: [40, 'Name must be atmost 40 characters'],
+    minlength: [10, 'Name must be atleast 10 characters']
   },
   duration: {
     type: Number,
@@ -17,11 +19,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty must be easy, medium or difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0']
   },
   ratingsQuantity: {
     type: Number,
@@ -31,7 +39,17 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        // The this keyword only points to 
+        // current doc on NEW doc creation
+        return val < this.price;
+      },
+      message: 'Discount price must be less than regular price'
+    }
+  },
   summary: {
     type: String,
     trim: true,
